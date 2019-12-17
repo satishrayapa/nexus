a8045526e280fb742e1bcab0585be29b06484a1a
Kakarlapudi <satishrayapa@gmail.com>
2019-12-17 18:25:36 +0530

    test variables

--- deployment/ConfigureStackName.ps1
+++ deployment/ConfigureStackName.ps1
@@ -18,4 +18,4 @@ else {
 
 Write-Host "##vso[task.setvariable variable=PipelineStackName;]$stackName"
 #$namee = [Environment]::GetEnvironmentVariable("PipelineStackName")
-#Write-Host "$stackName"
+Write-Host "$stackName"
