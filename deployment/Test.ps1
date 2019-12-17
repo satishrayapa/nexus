c00f730165248e97afa85a13b6d39029b3c5a713
Kakarlapudi <satishrayapa@gmail.com>
2019-12-17 18:57:20 +0530

    add isoutput

--- deployment/Test.ps1
+++ deployment/Test.ps1
@@ -8,5 +8,5 @@
 $VMName = [Environment]::GetEnvironmentVariable("Build_SourceBranch")
 $ResourceGroupName = [Environment]::GetEnvironmentVariable("name")
 #$con = [Environment]::GetEnvironmentVariable("Global_country")
-Write-Host "##vso[task.setvariable variable=city;]$ResourceGroupName"
+Write-Host "##vso[task.setvariable variable=city;isOutput=true]$ResourceGroupName"
 Write-Host "city name: $city"
