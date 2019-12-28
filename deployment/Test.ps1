9ba2e19ff101a50068476e31f4c99ffa246914ac
Kakarlapudi <satishrayapa@gmail.com>
2019-12-28 12:02:22 +0530

    change the double quote

--- deployment/Test.ps1
+++ deployment/Test.ps1
@@ -1,10 +1,9 @@
- param(    
-    [Parameter(Mandatory = $true)]   [String]$VMName )
-
+#param([Parameter(Mandatory = $true)]   [String]$VMName )
+param($VMName)
 
 
 #$name = [Environment]::GetEnvironmentVariable("name")
 #$ResourceGroupName = [Environment]::GetEnvironmentVariable("name")
 #$con = [Environment]::GetEnvironmentVariable("Global_country")
 #Write-Host "##vso[task.setvariable variable=city;isOutput=true]$name"
-Write-Host "$VMName"
+Write-Host $VMName
