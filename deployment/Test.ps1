50572abdbb5574d2779a7f264d091ea29ded2945
Kakarlapudi <satishrayapa@gmail.com>
2019-12-28 12:37:18 +0530

    try to access

--- deployment/Test.ps1
+++ deployment/Test.ps1
@@ -3,7 +3,8 @@ param($VMName)
 
 
 #$name = [Environment]::GetEnvironmentVariable("name")
-#$ResourceGroupName = [Environment]::GetEnvironmentVariable("name")
+$ResourceGroupName = [Environment]::GetEnvironmentVariable("aryan" +"_"+ "PipelineStackName")
 #$con = [Environment]::GetEnvironmentVariable("Global_country")
 #Write-Host "##vso[task.setvariable variable=city;isOutput=true]$name"
 Write-Host $VMName
+Write-Host $ResourceGroupName
